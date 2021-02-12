6a0e424016a599da3e67ab338bba402c318a49a7
julio1927 <jrivas6219@conestogac.on.ca>
2021-02-11 23:25:55 -0500

    Created react app using npx

---
+++ kj-ecommerce/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h1">KJ Ecommerce</div>
+    </div>
+  );
+}
+
+export default App;
