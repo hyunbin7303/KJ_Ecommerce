7cecf11cb54a5af89d6182d6de58b0e08629d712
julio1927 <jrivas6219@conestogac.on.ca>
2021-02-13 02:21:24 -0500

    created list and listitem component

--- kj-ecommerce/src/App.js
+++ kj-ecommerce/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import List from  "./Components/List/List";
 
 function App() {
   return (
     <div className="App">
       <div className="h1">KJ Ecommerce</div>
+      <List></List>
     </div>
   );
 }
---
+++ kj-ecommerce/src/Components/ListItem/ListItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ListItemContainer } from './ListItemElements/ListItemElements';
+
+
+function ListItem() {
+    return (
+        <div>
+            <ListItemContainer>
+                Nani
+            </ListItemContainer>
+        </div>
+    )
+}
+
+export default ListItem
