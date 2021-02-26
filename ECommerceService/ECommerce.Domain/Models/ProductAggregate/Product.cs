4c6eec35022e3c39e912bffb9a70d546e936c994
kevin <hyunbin7303@gmail.com>
2021-02-25 21:44:17 -0500

    Backend - DTO for Get Request Query. #23

--- ECommerceService/ECommerce.Domain/Models/ProductAggregate/Product.cs
+++ ECommerceService/ECommerce.Domain/Models/ProductAggregate/Product.cs
@@ -2,8 +2,6 @@
 {
     public class Product : Entity
     {
-
-        public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int SupplierId { get; set; }
---
+++ ECommerceService/ECommerce.Query/ProductDetailsDTO.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ECommerce.Query
+{
+    public class ProductDetailsDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double? UnitPrice { get; set; }
+        public string UnitsInStock { get; set; }
+
+
+        //Got source from here: https://mentormate.com/blog/service-and-repository-layer-interaction-in-c/
+        public static Expression<Func<Product, ProductDetailsDTO>> ProductSelector
+        {
+            get
+            {
+                return p => new ProductDetailsDTO()
+                {
+                    Name = p.Name,
+                    Description = p.Description,
+                    UnitPrice = p.UnitPrice,
+                    UnitsInStock = p.UnitsInStock,
+                };
+            }
+        }
+
+
+    }
+}
