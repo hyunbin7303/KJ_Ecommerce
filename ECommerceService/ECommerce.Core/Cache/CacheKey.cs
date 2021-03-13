4e0e3264276f7bd3c27d7646cce98a6f7fcc38f5
kevin <hyunbin7303@gmail.com>
2021-03-12 20:48:08 -0500

    Remove & update CacheKey.

---
+++ ECommerceService/ECommerce.Core/Cache/CacheKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Core.Cache
+{
+    // Should I change it to partial class?
+    //check this link later for caching implementation : https://github.com/nopSolutions/nopCommerce/blob/develop/src/Libraries/Nop.Core/Caching/CacheKey.cs
+    public class CacheKey
+    {
+        public string Key { get; protected set; }
+        public List<string> Prefixes { get; protected set; } = new List<string>();
+        public CacheKey(string key, params string[] prefixes)
+        {
+           
+        }
+    }
+}
