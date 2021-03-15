d1ff5f2def8405ba92c93f73c753c2076d637ee5
kevin <hyunbin7303@gmail.com>
2021-03-14 22:25:13 -0400

    Change some structures.

--- ECommerceService/ECommerceService.Test/IntegrationTest.cs
+++ ECommerceService/ECommerceService.Test/IntegrationTest.cs
@@ -1,20 +1,48 @@
 using ECommerce.AzureStorage;
+using Microsoft.AspNetCore.Mvc.Testing;
 using NUnit.Framework;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ECommerceService.Test
 {
-    public class Tests
+    public class IntegrationTest
     {
-        [SetUp]
-        public void Setup()
+        protected readonly HttpClient _client;
+        private readonly WebApplicationFactory<Startup> _factory;
+        public IntegrationTest(WebApplicationFactory<Startup> factory)
         {
+            _factory = factory;
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
         }
-
         [Test]
-        public void Test1()
+        [TestCase("/")]
+        [TestCase("/Index")]
+        [TestCase("/About")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentTypeAsync(string url)
         {
-            Assert.Pass();
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var response = await client.GetAsync(url);
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.AreEqual("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+
+        protected async Task AuthenticateAsync()
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+        private async Task<string> GetJwtAsync()
+        {
 
         }
+
+
     }
 }
