a5f39384325d34869edfa38895866527e2e83a1b
kevin <hyunbin7303@gmail.com>
2021-04-03 15:21:09 -0400

    merge all script and clean up.

--- SQLScript/Tables.sql
+++ SQLScript/Tables.sql
@@ -1,50 +1,132 @@
-use [MainEcommerceDB]
+USE [MainEcommerceDB]
 GO
+
+/****** Object:  Table [dbo].[Categories]    Script Date: 2021-03-29 10:05:07 PM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Product](
-	[Id] [nvarchar](50) NOT NULL,
-	[Name] [nvarchar](50) NULL,
-	[Description] [nvarchar](200),
-	[Customer] [nvarchar](50) NULL,
-	[ProductFormat] [nvarchar](50) NULL,
-	[QuantityPerUnit] [int] NULL,
-	[UnitPrice] [float] NULL,
-	[Discount] [float] NULL,
-	[UnitsInStock] [nvarchar](10) NULL,
-	[CategoryId] [int] NULL,
-	[ImageAddress][nvarchar](100) NULL,
-	[Note][nvarchar](200) Null,
- CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+
+CREATE TABLE [dbo].[Categories](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[ProductId] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+USE [MainEcommerceDB]
+GO
+
+/****** Object:  Table [dbo].[Customers]    Script Date: 2021-03-29 10:04:30 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customers](
+	[CustomerId] [nvarchar](450) NOT NULL,
+	[FirstName] [nvarchar](100) NOT NULL,
+	[LastName] [nvarchar](max) NULL,
+	[Address] [nvarchar](max) NULL,
+	[ZipCode] [nvarchar](max) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+
+/****** Object:  Table [dbo].[Invoices]    Script Date: 2021-03-29 10:00:43 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Invoices](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Date] [datetime2](7) NOT NULL,
+	[CustomerId] [nvarchar](450) NULL,
+ CONSTRAINT [PK_Invoices] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-
-
-USE [MainEcommerceDB]
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_Customers_CustomerId] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([CustomerId])
 GO
 
-/****** Object:  Table [dbo].[Category]    Script Date: 2021-02-12 5:18:19 PM ******/
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_Customers_CustomerId]
+GO
+
+
+/****** Object:  Table [dbo].[InvoiceItems]    Script Date: 2021-03-29 10:04:05 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Category](
-	[Id] [int] NOT NULL,
-	[Name] [nvarchar](50) NULL,
-	[ProductId] [nvarchar](50) NULL
-) ON [PRIMARY]
+CREATE TABLE [dbo].[InvoiceItems](
+	[InvoiceItemId] [int] IDENTITY(1,1) NOT NULL,
+	[InvoiceId] [int] NOT NULL,
+	[Code] [nvarchar](max) NULL,
+ CONSTRAINT [PK_InvoiceItems] PRIMARY KEY CLUSTERED 
+(
+	[InvoiceItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-ALTER SCHEMA NewSchema TRANSFER [OldSchema].[TableName]
-Go
-alter schema Discount TRANSFER dbo.Products
-Go
+ALTER TABLE [dbo].[InvoiceItems]  WITH CHECK ADD  CONSTRAINT [FK_InvoiceItems_Invoices_InvoiceId] FOREIGN KEY([InvoiceId])
+REFERENCES [dbo].[Invoices] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[InvoiceItems] CHECK CONSTRAINT [FK_InvoiceItems_Invoices_InvoiceId]
+GO
+
+
+/****** Object:  Table [dbo].[Products]    Script Date: 2021-03-29 9:59:28 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products](
+	[Id] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[Description] [nvarchar](max) NULL,
+	[SupplierId] [int] NOT NULL,
+	[CustomerId] [nvarchar](max) NULL,
+	[ProductFormat] [nvarchar](max) NULL,
+	[QuantityPerUnit] [int] NULL,
+	[UnitPrice] [float] NULL,
+	[UnitsInStock] [nvarchar](max) NULL,
+	[CategoryId] [int] NULL,
+	[ImageAddress] [nvarchar](max) NULL,
+	[Note] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
 
