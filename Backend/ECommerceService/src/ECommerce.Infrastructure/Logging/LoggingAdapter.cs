21e4b58dc798ce63f32c78ab52250a6f0c0db828
kevin <hyunbin7303@gmail.com>
2021-04-25 17:45:26 -0400

    update Logging Adapter.

---
+++ Backend/ECommerceService/src/ECommerce.Infrastructure/Logging/LoggingAdapter.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+
+// source from : https://github.com/dotnet-architecture/eShopOnWeb/blob/master/src/ApplicationCore/Interfaces/IAppLogger.cs
+namespace ECommerce.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+    }
+}
