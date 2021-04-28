a1a0ff1e5a061b8c5b922d59c743d2e24989cd35
Habib Shakibanejad <hs_m19@hotmail.com>
2021-04-27 22:26:57 -0400

    fix address constructor

--- Backend/ECommerceService/src/ECommerce.Core/Models/Address.cs
+++ Backend/ECommerceService/src/ECommerce.Core/Models/Address.cs
@@ -8,20 +8,20 @@ namespace ECommerce.Core.Models
 {
     public class Address : Entity<int>
     {
-        public Address(int id, string contactname, string phone, string address1, string city, string province, string country, string address2 = null, string desc = null)
+        public Address(int id, string contactName, string phone, string address1, string city, string province, string country, string address2 = null, string description = null)
         {
             Id = id;
-            ContactName = contactname;
+            ContactName = contactName;
             Phone = phone;
             Address1 = address1;
             Address2 = address2;
             City = city;
             Province = province;
             Country = country;
-            Description = desc;
+            Description = description;
         }
-        public string ContactName { get; }
-        public string Phone { get; }
+        public string ContactName { get;}
+        public string Phone { get;  }
         public string Address1 { get;  }
         public string? Address2 { get;  }
         public string City { get;  }
