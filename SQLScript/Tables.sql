bd80a6a5699052b07db8e191c56a5a05eb89fb63
Kookhwan <tatata1358@gmail.com>
2021-05-08 16:42:32 -0400

    Change filed name (ProductID -> Description)

--- SQLScript/Tables.sql
+++ SQLScript/Tables.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS [dbo].[Category];
 CREATE TABLE [dbo].[Category](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
-	[ProductId] [nvarchar](max) NULL,
+	[Description] [nvarchar](max) NULL,
  CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -306,12 +306,12 @@ GO
 
 ALTER TABLE [OrderItem] ADD CONSTRAINT FK_OrderId FOREIGN KEY (OrderId) REFERENCES [Order](id);
 ALTER TABLE [OrderItem] ADD CONSTRAINT FK_ProductId FOREIGN KEY (ProductId) REFERENCES [Product](id);
-ALTER TABLE [Product]   ADD CONSTRAINT FK_CategoryId FOREIGN KEY (CategoryId) REFERENCES [Category](id);
-ALTER TABLE [Product]   ADD CONSTRAINT FK_VendorId FOREIGN KEY   (VendorId) REFERENCES [Vendor](id);
-ALTER TABLE [CartItem]   ADD CONSTRAINT FK_CartId FOREIGN KEY   (CartId) REFERENCES [Cart](id);
-ALTER TABLE [Payment]   ADD CONSTRAINT FK_PaymentMethodId FOREIGN KEY   (PaymentMethodId) REFERENCES [PaymentMethod](id);
-ALTER TABLE [ProductAttribute]   ADD CONSTRAINT FK_ProductAttribute_ProudctId FOREIGN KEY   (ProductId) REFERENCES [Product](id);
-ALTER TABLE [ProductAttribute]   ADD CONSTRAINT FK_ProductAttribute_AttributeId FOREIGN KEY (AttributeId) REFERENCES [Attribute](id);
+ALTER TABLE [Product] ADD CONSTRAINT FK_CategoryId FOREIGN KEY (CategoryId) REFERENCES [Category](id);
+ALTER TABLE [Product] ADD CONSTRAINT FK_VendorId FOREIGN KEY (VendorId) REFERENCES [Vendor](id);
+ALTER TABLE [CartItem] ADD CONSTRAINT FK_CartId FOREIGN KEY (CartId) REFERENCES [Cart](id);
+ALTER TABLE [Payment] ADD CONSTRAINT FK_PaymentMethodId FOREIGN KEY (PaymentMethodId REFERENCES [PaymentMethod](id);
+ALTER TABLE [ProductAttribute] ADD CONSTRAINT FK_ProductAttribute_ProudctId FOREIGN KEY (ProductId) REFERENCES [Product](id);
+ALTER TABLE [ProductAttribute] ADD CONSTRAINT FK_ProductAttribute_AttributeId FOREIGN KEY (AttributeId) REFERENCES [Attribute](id);
 
 
 
