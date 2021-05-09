eaecf2041e13fdd6ceb045fe6662eaebc17ed52e
Kookhwan <tatata1358@gmail.com>
2021-05-08 21:15:28 -0400

    Add features into Interface Category

--- Backend/ECommerceService/src/ECommerce.Core/Interfaces/ICategoryRepository.cs
+++ Backend/ECommerceService/src/ECommerce.Core/Interfaces/ICategoryRepository.cs
@@ -1,13 +1,16 @@
 ﻿using ECommerce.Core.Models.ProductAggregate;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ECommerce.Core.Interfaces
 {
     public interface ICategoryRepository : IGenericRepository<Category>
     {
-
-
+        Task<IEnumerable<Category>> GetCategoryAsync();
+        Task<Category> GetCategoryById(int categoryId);
+        Task<IEnumerable<Category>> GetCategoryByNameAsync(string categoryName);
     }
 }
