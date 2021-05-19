8a64c9e4306217b09918168c840fd5e98b2c591c
Kookhwan <tatata1358@gmail.com>
2021-05-18 23:03:33 -0400

    fix Alter statement

--- SQLScript/Tables.sql
+++ SQLScript/Tables.sql
@@ -1,4 +1,3 @@
-
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -308,7 +307,7 @@ ALTER TABLE [OrderItem] ADD CONSTRAINT FK_ProductId FOREIGN KEY (ProductId) REFE
 ALTER TABLE [Product] ADD CONSTRAINT FK_CategoryId FOREIGN KEY (CategoryId) REFERENCES [Category](id);
 ALTER TABLE [Product] ADD CONSTRAINT FK_VendorId FOREIGN KEY (VendorId) REFERENCES [Vendor](id);
 ALTER TABLE [CartItem] ADD CONSTRAINT FK_CartId FOREIGN KEY (CartId) REFERENCES [Cart](id);
-ALTER TABLE [Payment] ADD CONSTRAINT FK_PaymentMethodId FOREIGN KEY (PaymentMethodId REFERENCES [PaymentMethod](id);
+ALTER TABLE [Payment] ADD CONSTRAINT FK_PaymentMethodId FOREIGN KEY (PaymentMethodId) REFERENCES [PaymentMethod](id);
 ALTER TABLE [ProductAttribute] ADD CONSTRAINT FK_ProductAttribute_ProudctId FOREIGN KEY (ProductId) REFERENCES [Product](id);
 ALTER TABLE [ProductAttribute] ADD CONSTRAINT FK_ProductAttribute_AttributeId FOREIGN KEY (AttributeId) REFERENCES [Attribute](id);
 
