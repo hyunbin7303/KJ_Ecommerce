79af20e8c3a15b785896a00ce93ca7cf7a061d80
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-06-01 17:10:30 -0400

    SearchBar

--- Frontend/kj-ecommerce/src/Components/NavBar/NarBar.js
+++ Frontend/kj-ecommerce/src/Components/NavBar/NarBar.js
@@ -1,27 +1,59 @@
 import React from "react";
 import {
+  CartDiv,
   NavBarContainer,
   NavBarHeader,
   NavBarLogoContainer,
   NavBarLogoImg,
+  OrdersDiv,
   SearchBarContainer,
+  SearchBarIconContainer,
   SearchBarInput,
+  UserAccountDiv,
+  UserInfoContainer,
+  UserInfoWrapper,
 } from "./NavBarElements/NavBarElements";
+import { MenuIcon, SearchIcon, CartIcon } from "@heroicons/react/outline";
+import classes from "./NavBar.module.css";
 import Logo from "../../Assets/Images/KJ Ecommerce.png";
 
 const NarBar = () => {
   return (
     <NavBarHeader>
       <NavBarContainer>
-          {/* Nav Bar Logo */}
+        {/* Nav Bar Logo Section*/}
         <NavBarLogoContainer>
           <NavBarLogoImg src={Logo} />
         </NavBarLogoContainer>
 
-        {/* Search Bar */}
+        {/* Search Bar Section*/}
         <SearchBarContainer>
-          <SearchBarInput />
+          <SearchBarInput type="text" />
+          <SearchBarIconContainer>
+            <SearchIcon className={classes.SearchBar} />
+          </SearchBarIconContainer>
         </SearchBarContainer>
+
+        {/* Nav Bar UserInfo Section */}
+        <UserInfoContainer>
+          {/* Surrounding Wrapper */}
+          <UserInfoWrapper>
+            {/* User Account Div */}
+            <UserAccountDiv>
+
+            </UserAccountDiv>
+
+            {/* Orders Div */}
+            <OrdersDiv>
+
+            </OrdersDiv>
+
+            {/* Cart Div */}
+            <CartDiv>
+              
+            </CartDiv>
+          </UserInfoWrapper>
+        </UserInfoContainer>
       </NavBarContainer>
     </NavBarHeader>
   );
--- Frontend/kj-ecommerce/src/Components/NavBar/NavBarElements/NavBarElements.js
+++ Frontend/kj-ecommerce/src/Components/NavBar/NavBarElements/NavBarElements.js
@@ -17,7 +17,7 @@ export const NavBarLogoContainer = styled.div`
   align-items: center;
   margin-top: 0.5rem;
 
-  @media screen and (min-width: 640px) {
+  @media (min-width: 640px) {
     flex-grow: 0;
   }
 `;
@@ -30,9 +30,54 @@ export const NavBarLogoImg = styled.img`
 `;
 
 export const SearchBarContainer = styled.div`
+  display: none;
+  flex-grow: 1;
+  background: #54647a;
+  align-items: center;
+  height: 2.5rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
 
+  @media (min-width: 640px) {
+    display: flex;
+  }
 `;
 
+export const SearchBarIconContainer = styled.div``;
+
 export const SearchBarInput = styled.input`
+  padding: 0.68rem;
+  height: 100%;
+  width: 1.5rem;
+  flex-grow: 1;
+  flex-shrink: 1;
+  border-top-left-radius: 0.375rem;
+  border-bottom-left-radius: 0.375rem;
+
+  &:focus {
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+  }
+`;
+
+
+export const UserInfoContainer = styled.div`
+  
+`;
+
+export const UserInfoWrapper = styled.div`
+  
+`;
+
+
+export const UserAccountDiv = styled.div`
+  
+`;
+
+export const OrdersDiv = styled.div`
+  
+`;
+
+export const CartDiv = styled.div`
   
 `;
