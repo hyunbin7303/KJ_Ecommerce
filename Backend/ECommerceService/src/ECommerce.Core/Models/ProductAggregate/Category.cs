51d42d99098ed60e1f030a73a14f4259d897ee04
Kookhwan <tatata1358@gmail.com>
2021-06-27 19:04:13 -0400

    Update Category.cs

--- Backend/ECommerceService/src/ECommerce.Core/Models/ProductAggregate/Category.cs
+++ Backend/ECommerceService/src/ECommerce.Core/Models/ProductAggregate/Category.cs
@@ -11,9 +11,10 @@ namespace ECommerce.Core.Models.ProductAggregate
         public Category()
         {
         }
+        public string id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public int ParentId { get; set; }
+        public string ParentId { get; set; }
         public string Description { get; set; }
         public bool Active { get; set; }
     }
