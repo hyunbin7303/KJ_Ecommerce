42f946fcc8ccbb91a558b81c1b58b9aefebf59c9
Kookhwan <tatata1358@gmail.com>
2021-08-14 16:04:51 -0400

    Update field property

--- Backend/ECommerceService/src/ECommerce.Infrastructure/20210606143309_TestMigration.cs
+++ Backend/ECommerceService/src/ECommerce.Infrastructure/20210606143309_TestMigration.cs
@@ -97,12 +97,11 @@ namespace ECommerce.Infrastructure.Migrations
                 name: "Category",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ParentId = table.Column<int>(type: "int", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Id = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    ParentId = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", nullable: true),
                     Active = table.Column<bool>(type: "bit", nullable: false)
                 },
                 constraints: table =>
